fabe24cd41127820919fa94e6ad804e61cd07e4d
User <User@DESKTOP-5IQL2LK.realtek>
2020-12-20 15:13:58 +0800

    First Commit

---
+++ Assignment-1A/src/assignment1A/Documentary.java
@@ -0,0 +1,31 @@
+package assignment1A;
+
+public class Documentary {
+
+	String title,genre,date;
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getGenre() {
+		return this.genre;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getDate() {
+		return this.date;
+	}
+	
+}
--- Assignment-1A/src/assignment1A/ShortVideoMain.java
+++ Assignment-1A/src/assignment1A/ShortVideoMain.java
@@ -5,14 +5,23 @@ public class ShortVideoMain {
 	public static void main (String [] args) {
 		
 		Genre g = new Genre();
+		Documentary d = new Documentary();
  		
 		g.setTitle("Spongebob");
 		g.setGenre("Cartoon");
 		g.setDate("20-December-2020");
 		
+		d.setTitle("National Geography : World Climate");
+		d.setGenre("Documentary");
+		d.setDate("19-December-2020");
+		
 		System.out.println("Short Video");
 		System.out.println("Name : " + g.getTitle());
 		System.out.println("Genre : " + g.getGenre());
 		System.out.println("Date Release : " + g.getDate());
+		
+		System.out.println("\nName : " + d.getTitle());
+		System.out.println("Genre : " + d.getGenre());
+		System.out.println("Date Release : " + d.getDate());
 	}
 }
